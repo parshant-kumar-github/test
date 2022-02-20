cf1a028081d5ab95b15fcb6fea36ca80b1090cc2
Parshant Kumar <65731730+parshant-kumar@users.noreply.github.com>
2022-02-21 01:19:23 +0530

    Update py1.py

--- py1.py
+++ py1.py
@@ -1,3 +1,21 @@
+def sunun(y):
+  print("wofhwbfw")
+def fasdfaun(x):
+  print("sfsnsln")
+  print(f"x : {x}")
+  
+print("aenfkajbfbfs")
+      
+fun(23)
+
+print("aljekas f")
+fun(3423)
+      
+fun(234334)
+
+dun("ksjhs")     
+      
+fun(23)
 def dun(y):
   print("wofhwbfw")
 def fun(x):
