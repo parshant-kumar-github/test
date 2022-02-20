bfc5693c92777bf58c5afa262b2d0a61dea60bd3
parshant-kumar-github <parshant.kumar@searce.com>
2022-02-21 01:13:34 +0530

    new stuff

--- py1.py
+++ py1.py
@@ -1,10 +1,14 @@
+def dun(y):
+  print("wofhwbfw")
 def fun(x):
   print(f"x : {x}")
   
-print("aenfkajbfbfs)
+print("aenfkajbfbfs")
       
 fun(23)
+
+print("aljekas f")
       
 
-      
+dun("ksjhs")     
      
