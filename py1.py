8ac0ca8304bb9ed4cf92c12aea3a8ddf7518e86e
parshant-kumar-github <parshant.kumar@searce.com>
2022-02-21 01:50:18 +0530

    dfew

--- py1.py
+++ py1.py
@@ -1,3 +1,5 @@
+def fun(x):
+  print(x)
 def sunun(y):
   print("wofhwbfw")
 def fasdfaun(x):
@@ -12,6 +14,62 @@ print("aljekas f")
 fun(3423)
       
 fun(234334)
+
+def sunun(y):
+  print("wofhwbfw")
+def fasdfaun(x):
+  print("sfsnsln")
+  print(f"x : {x}")
+  
+print("aenfkajbfbfs")
+      
+fun(23)
+
+print("aljekas f")
+fun(3423)
+      
+fun(234334)
+
+fun("ksjhs")     
+      
+fun(23)
+def dun(y):
+  print("wofhwbfw")
+def fun(x):
+  print("sfsnsln")
+  print(f"x : {x}")
+  
+def dsefsfun(y):
+  print("wofhwbfw")
+def funsfsf(x):
+  print("sfsnsln")
+  print(f"x : {x}")
+  
+print("aenfkajbfbfs")
+      
+fun(23)
+
+print("aljekas f")
+fun(3423)
+      
+fun(234334)
+
+dun("ksjhs")     
+      
+fun(23)
+        
+print("aenfkajbfbfs")
+      
+fun(23)
+
+print("aljekas f")
+fun(3423)
+      
+fun(234334)
+
+dun("ksjhs")     
+      
+fun(23)
 
 dun("ksjhs")     
       
